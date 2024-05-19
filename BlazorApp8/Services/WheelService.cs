955f36d6e6f228a674034eb3e4c1cf285e96b261
Matt Storz <mstorz57@gmail.com>
2024-05-19 14:16:20 -0600

    initial commit

---
+++ BlazorApp8/Services/WheelService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorApp8.Services
+{
+    public class WheelService
+    {
+
+        public event EventHandler<int> OnWheel;
+        private readonly IJSRuntime _JSRuntime;
+
+        public WheelService(IJSRuntime JSRuntime)
+        {
+            _JSRuntime = JSRuntime;
+            _JSRuntime.InvokeVoidAsync("RegisterWheelService", DotNetObjectReference.Create(this));
+        }
+
+
+        [JSInvokable("JsOnWheel")]
+        public void JsOnWheel(int y)
+        {
+            OnWheel?.Invoke(this, y);
+        }
+    }
+}
